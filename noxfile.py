97fb435d843528e3054d1aef2dbfb58c7248b391
Jon T <jjt@tutanota.com>
2021-02-18 18:39:59 +0100

    add nox

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox
+
+@nox.session(python=["3.9", "3.8"])
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.run("pytest", "--cov")
+
+locations = "grakn_dev_utils", "tests", "noxfile.py"
+
+@nox.session(python=["3.9", "3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
