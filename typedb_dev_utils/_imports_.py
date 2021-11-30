04632e3e8af5e0d53a1b0d7d4c6d8e50f3b32a5a
Jon Thompson <jjt@tutanota.com>
2021-11-30 16:09:41 +0100

    commit all

---
+++ typedb_dev_utils/_imports_.py
@@ -0,0 +1,16 @@
+
+from .typedb_dev_utils import del_db, init_db, ls_types, def_attr_type, get_type_owns, def_rel_type, get_type_plays, insert_data, ls_instances, modify_each_concept
+
+__all__ = [
+    # core 
+    "del_db",
+    "init_db",
+    "ls_types",
+    "def_attr_type",
+    "get_type_owns",
+    "def_rel_type",
+    "get_type_plays",
+    "insert_data",
+    "ls_instances",
+    "modify_each_concept",
+]
